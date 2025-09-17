cf77e89a0721d92561eacffbe2a45920d2601040
Sandile <sandilembatha89@gmail.com>
2025-09-17 22:35:59 +0200

    Add Product details page

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,10 +1,13 @@
 import "./ProductCard.css"
+import { useNavigate } from "react-router-dom";
 
 function ProductCard({ product, style }) {
+    const navigate = useNavigate();
  return (
         <div
             className="product-card"
             style={style}
+            onClick={() => navigate("/details", { state: {productId: product.id } })}
         >
             <img src={`data:image/png;base64,${product.imageid.content}`} alt={product.name} className="picture" />
             <div className="info">
---
+++ src/components/ProductDetails.jsx
@@ -0,0 +1,56 @@
+import "./ProductDetails.css"
+import React, { useEffect, useState } from "react";
+import { useLocation, useParams, useNavigate } from "react-router-dom";
+
+function ProductDetails() {
+  const location = useLocation();
+  const params = useParams();
+  const navigate = useNavigate();
+  const stateName = location?.state?.productId;
+ const paramName = params?.name;
+  const productId = stateName || paramName || "";
+
+    const [productDetails, setProductDetails] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+  const API_BASE = "http://localhost:8080/products";
+    useEffect(() => {
+    const fetchProductDetails = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(`${API_BASE}/${encodeURIComponent(productId)}`);
+        const data = await res.json();
+        setProductDetails(data);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProductDetails();
+    }, [productId]);
+
+    if (!productDetails) return <div>No product selected.</div>;
+    if (loading) return <div>Loading...</div>;
+    if (!productDetails) return <div>Product not found.</div>;
+
+    return (
+
+        <div className="product-details">
+             <button
+        type="button"
+        className="back-button"
+        onClick={() => navigate("/")}
+        aria-label="Go back to list"
+      >
+        ← Back
+      </button>
+
+            <h1>{productDetails.name}</h1>
+            <img src={`data:image/png;base64,${productDetails.imageid.content}`} alt={productDetails.name} className="picture" />
+            <p>{productDetails.description}</p>
+        </div>
+    );
+}
+
+export default ProductDetails;
